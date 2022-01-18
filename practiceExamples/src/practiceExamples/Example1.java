2c859e0b8b5605e1bb254fe0c1735cae2ff6b98c
RAHUL <RAHUL@DESKTOP-Q0NCB85>
2022-01-18 18:56:26 +0530

    First commit

---
+++ practiceExamples/src/practiceExamples/Example1.java
@@ -0,0 +1,24 @@
+package practiceExamples;
+
+import java.util.Scanner;
+
+public class Example1 {
+ 
+public static void main(String[] yogesh) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the Number :");
+	int n = sc.nextInt();
+	
+	if (n > 0) {
+		System.out.println("Number is positive");
+	}
+	else if (n<0){
+		System.out.println("NUmber is negative");
+	}
+	else {
+		System.out.println("Number is zero");
+	}
+
+}
+   
+}
---
+++ practiceExamples/src/practiceExamples/Example2.java
@@ -0,0 +1,17 @@
+package practiceExamples;
+
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a = 1;
+		int b = 5;
+		int c = 3;
+		
+
+	}
+
+}
